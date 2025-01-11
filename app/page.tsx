82870e698d9636f7275dbc807a08a99b91d29942
SeraAres09 <seraares202009@gmail.com>
2025-01-11 12:35:18 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import GiderKayitFormu from './components/GiderKayitFormu'
+
+export default function Home() {
+  return (
+    <main className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Gider Kayıt Sayfası</h1>
+      <GiderKayitFormu />
+    </main>
+  )
+}
+
